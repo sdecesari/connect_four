c90b7a538df416bdd8eaa2be7814c09ae640df6c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-30 18:39:19 -0700

    Add column names

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Board
   attr_reader :height, :column
   def initialize(height, column)
@@ -7,11 +8,21 @@ class Board
   end
 
   def create_board
-    board = []
-    @column.times do
-      board << Array.new[][]
-    end
+    board = Array.new(@height) {Array.new(@column) {"."}}
+
     board
   end
 
+  def display_board
+    create_board.each do |line|
+      puts line.join("")
+    end
+  end
+
+
 end
+
+print 'ABCDEFG'
+game_board = Board.new(7,6)
+game_board.create_board
+game_board.display_board
