41e69e88530fc9288e0dfcd6f906705a0b1792be
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-31 11:45:18 -0700

    Fix message and remove instance variables

--- lib/board.rb
+++ lib/board.rb
@@ -1,28 +1,29 @@
 require 'pry'
 class Board
-  attr_reader :height, :column, :board
-  def initialize(height, column)
-    @height = height
-    @column = column
+  attr_reader :board
+  def initialize
     @board = board
 
   end
 
   def create_board
-    board = Array.new(@height) {Array.new(@column) {"."}} #why is array capitalized
-      board.unshift(["ABCDEFG"])
-    board
+  @board = Array.new(6) {Array.new(7) {"."}}
+    @board.unshift(["ABCDEFG"])
   end
 
   def display_board
-    create_board.each do |line|
+    @board.each do |line|
       puts line.join("")
     end
   end
 
+  def welcome
+    p "Welcome to Connect Four!"
+  end
 end
 
-p "Welcome to Connect Four!"
-game_board = Board.new(7,7)
-game_board.create_board
-game_board.display_board
+
+# game_board = Board.new
+# game_board.welcome
+# game_board.create_board
+# game_board.display_board
