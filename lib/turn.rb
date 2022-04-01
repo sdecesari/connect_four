629da530b669fee0e2ba364ff39b29a8ab0fd9bc
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-01 02:30:02 -0700

    make Turn methods to put pieces on board

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,47 @@
 class Turn
-  attr_reader :board
+  attr_reader :gameboard, :user_column
 
-  def initialize(board)
-    @board = board
+  def initialize(gameboard)
+    @gameboard = gameboard
+    @board_hash = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G:6 }
+    @user_column = 0
+  end
+
+  def input_column
+    p "input a column A-G"
+    letter_column = gets().chomp!.to_sym
+    @user_column = @board_hash[letter_column]
   end
 
   def valid_column?
+    x = 6
+    while @gameboard.board[x][@user_column] == 'X' || @gameboard.board[x][@user_column] == 'O'
+      if x == 1
+        return false
+        break
+      end
+      x -= 1
+    end
+    true
+  end
 
+  def get_row
+    if valid_column?
+      row = 6
+      while @gameboard.board[row][@user_column] == 'X' || @gameboard.board[row][@user_column] == 'O'
+        row -= 1
+      end
+    else
+      input_column
+    end
+    row
   end
 
   def place_piece
+      @gameboard.board[get_row][@user_column] = 'X'
+  end
+
+  def computer_place_piece
+      @gameboard.board[get_row][@user_column] = 'O'
   end
 end
