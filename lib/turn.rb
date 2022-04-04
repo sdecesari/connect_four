99e5e232c9f1248a61114aeb67217765c4a26193
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-04 08:56:58 -0700

    add diagonal win condition and make row an instance variable

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,7 @@ class Turn
     @gameboard = gameboard
     @board_hash = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G:6 }
     @user_column = 0
+    @row = 0
   end
 
   def input_column
@@ -27,23 +28,25 @@ class Turn
 
   def get_row
     if valid_column?
-      row = 6
-      while @gameboard.board[row][@user_column] == 'X' || @gameboard.board[row][@user_column] == 'O'
-        row -= 1
+      @row = 6
+      while @gameboard.board[@row][@user_column] == 'X' || @gameboard.board[@row][@user_column] == 'O'
+        @row -= 1
       end
     else
       input_column
     end
-    row
+    @row
   end
 
   def place_piece
-      @gameboard.board[get_row][@user_column] = 'X'
+      @row = get_row
+      @gameboard.board[@row][@user_column] = 'X'
   end
 
   def computer_place_piece
       @user_column = rand(7)
-      @gameboard.board[get_row][@user_column] = 'O'
+      @row = get_row
+      @gameboard.board[@row][@user_column] = 'O'
   end
 
   def draw?
@@ -52,5 +55,35 @@ class Turn
     else
       true
     end
+  end
+
+  def diagonal_right_win
+    arr = []
+    k = 0
+    j = 1
+    while (@row - k) >= 1 && @user_column + k <= 6
+      arr << @gameboard.board[@row - k][@user_column + k]
+      k += 1
+    end
+    while (@row + j) <= 6 && @user_column - j >= 0
+      arr << @gameboard.board[@row + j][@user_column - j]
+      j += 1
+    end
+    arr.find_all{|x| x == "X"}.length == 4 || arr.find_all{|o| o == "O"}.length == 4
+  end
+
+  def diagonal_left_win
+    arr = []
+    k = 0
+    j = 1
+    while (@row - k) >= 1 && @user_column - k >= 0
+      arr << @gameboard.board[@row - k][@user_column - k]
+      k += 1
+    end
+    while (@row + j) <= 6 && @user_column + j >= 0
+      arr << @gameboard.board[@row + j][@user_column + j]
+      j += 1
+    end
+    arr.find_all{|x| x == "X"}.length == 4 || arr.find_all{|o| o == "O"}.length == 4
   end
 end
