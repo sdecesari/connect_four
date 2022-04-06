66a34c196a097ee5dd408c2646c82b268f69fbfa
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-06 10:21:24 -0700

    add condition for when user does not push p or q to play

--- lib/game.rb
+++ lib/game.rb
@@ -5,8 +5,8 @@ require 'pry'
 
 
 class Game
-  def initialize
-    @new_game = 'q'
+  def new_game(user_input)
+    @new_game = user_input
   end
 
   def welcome
@@ -17,28 +17,31 @@ class Game
   def new_connect_four
     welcome
     @new_game = gets().chomp!
-    if @new_game != 'p'
+    if @new_game == 'q'
       p "Goodbye!"
       return
-    end
-    while @new_game == 'p'
-      game_board = Board.new
-      game_board.create_board
-      turn = Turn.new(game_board)
-      turn.row(6)
-      turn.user_column(0)
-      while !turn.win
-        turn.input_column
-        turn.place_piece
-        turn.gameboard.display_board
-        if turn.win
-          break
+    elsif @new_game == 'p'
+      while @new_game == 'p'
+        game_board = Board.new
+        game_board.create_board
+        turn = Turn.new(game_board)
+        turn.row(6)
+        turn.user_column(0)
+        while !turn.win
+          turn.input_column
+          turn.place_piece
+          turn.gameboard.display_board
+          if turn.win
+            break
+          end
+          turn.computer_place_piece
+          turn.gameboard.display_board
         end
-        turn.computer_place_piece
-        turn.gameboard.display_board
-      end
-      turn.winner
+        turn.winner
         new_connect_four
+      end
+    else
+      new_connect_four
     end
   end
 
